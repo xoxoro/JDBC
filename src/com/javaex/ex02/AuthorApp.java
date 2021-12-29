2f0497feca777858999a4d3e3167195863e5b9cb
심유정 <shimuniv@gmail.com>
2021-12-29 16:18:41 +0900

    ex02

--- src/com/javaex/ex02/AuthorApp.java
+++ src/com/javaex/ex02/AuthorApp.java
@@ -1,38 +1,43 @@
 package com.javaex.ex02;
 
+import java.util.List;
+
 public class AuthorApp {
 
-public static void main(String[] args) {
-		
+	public static void main(String[] args) {
+
+		List<AuthorVo> list ;
 		AuthorDao authorDao = new AuthorDao();
 		
-		//작가등록
+		// 작가등록
 		authorDao.authorInsert("이문열", "경북 영양");
-		
-		//작가등록
-		authorDao.authorInsert("박경리", "경상남도 통영");
-		
-		
-		List<AuthorVo> authorList = authorDao.authorSelect();
-		
-		
-		
-		//작가수정
-		//authorDao.authorUpdate(2, "박경리(수정)", "경상남동 통영(수정)");
-				
-		
-		//작가삭제 --->만들어보세요
-		//authorDao.authorDelete(1);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	}
 
+		// 작가등록
+		authorDao.authorInsert("박경리", "경상남도 통영");
+
+		// 작가등록
+		authorDao.authorInsert("유시민", "17대 국회의원");
+		// 출력
+		List<AuthorVo> authorList = authorDao.authorSelect();
+		for (int i = 0; i < authorList.size(); i++) {
+			AuthorVo vo = authorList.get(i);
+			System.out.println(vo.getAuthorId() + ", " + vo.getAuthorName() + ", " + vo.getAuthorDesc());
+		}
+
+		// 작가수정
+		authorDao.authorUpdate(2, "박경리(수정)", "경상남동 통영(수정)");
+
+		// 출력
+		list = authorDao.authorSelect();
+		for (int i = 0; i < list.size(); i++) {
+			AuthorVo vo = list.get(i);
+			System.out.println(vo.getAuthorId() + ", " + vo.getAuthorName() + ", " + vo.getAuthorDesc());
+		}
+		System.out.println("--------------------------------------------");
+
+		// 작가삭제 --->만들어보세요
+		// authorDao.authorDelete(1);
+
+	}
 
 }
